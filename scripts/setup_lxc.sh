4754d20a9aae4afd482795ca84f53fe195a7ef42
Kristian Klein <kristian.klein90@gmail.com>
2018-11-23 11:11:24 +0100

    Included cgroups_fix in setup_lxc and added bridge setup

--- scripts/setup_lxc.sh
+++ scripts/setup_lxc.sh
@@ -12,14 +12,12 @@
 if dpkg -s lxc > /dev/null 2>&1
 then
 	echo "LXC is already installed."
-	echo ""
 else
-	echo "Installing LXC..."
+	echo -n "Installing LXC... "
 	sudo apt-get install lxc -y > /dev/null
 	if [ $? = 0 ]
 	then
 		echo "LXC installed succesfully!"
-		echo ""
 	else
 		echo "Installation failed. Aborting."
 		exit 1
@@ -42,17 +40,51 @@ echo "lxc.id_map = u 0 $UIDstart $UIDlength" > ~/.config/lxc/default.conf
 echo "lxc.id_map = g 0 $GIDstart $GIDlength" >> ~/.config/lxc/default.conf
 echo "lxc.network.type = veth" >> ~/.config/lxc/default.conf
 echo "lxc.network.link = lxcbr0" >> ~/.config/lxc/default.conf
-echo "$USER veth lxcbr0 2" | sudo tee /etc/lxc/lxc-usernet > /dev/null
+echo "$USER veth lxcbr0 10" | sudo tee /etc/lxc/lxc-usernet > /dev/null
 
-# Fix for unprivileged containers not working on specific kernels
-# https://askubuntu.com/questions/654722/lxc-is-returning-an-error-when-creating-new-unprivileged-containers
-if [ $(cat /proc/sys/kernel/unprivileged_userns_clone) = 0 ]; then
-	echo "Kernel does not allow unprivileged containers. Adding fix."
-	echo "kernel.unprivileged_userns_clone=1" | sudo tee /etc/sysctl.d/80-lxc-userns.conf > /dev/null
-  sudo sysctl --system > /dev/null
-fi
+echo "Created ~/.config/lxc/default.conf and /etx/lxc/lxc-usernet."
 
-echo ""
+# Add user to required cgroups
+# https://github.com/NixOS/nixpkgs/issues/25754
+echo -n "Adding current user to required cgroups... "
+
+for d in /sys/fs/cgroup/*; do
+        f=$(basename $d)
+        if [ "$f" = "cpuset" ]; then
+                echo 1 | sudo tee -a $d/cgroup.clone_children > /dev/null;
+        elif [ "$f" = "memory" ]; then
+                echo 1 | sudo tee -a $d/memory.use_hierarchy > /dev/null;
+        fi
+        sudo mkdir -p $d/$USER
+        sudo chown -R $USER $d/$USER
+        echo $$ > $d/$USER/tasks
+done
+
+echo "Done."
+
+# Set up bridge using lxc-net
+# https://wiki.debian.org/LXC/SimpleBridge
+echo -n "Setting up network bridge using lxc-net... "
+
+cat << EOF | sudo tee /etc/default/lxc-net > /dev/null
+USE_LXC_BRIDGE="true"
+LXC_BRIDGE="lxcbr0"
+LXC_ADDR="10.0.3.1"
+LXC_NETMASK="255.255.255.0"
+LXC_NETWORK="10.0.3.0/24"
+LXC_DHCP_RANGE="10.0.3.2,10.0.3.254"
+LXC_DHCP_MAX="253"
+LXC_DHCP_CONFILE=""
+LXC_DOMAIN=""
+EOF
+
+echo "Done."
+
+echo -n "Enabling and restarting lxc-net..."
+sudo systemctl enable lxc-net > /dev/null 2>&1
+sudo systemctl start lxc-net > /dev/null 2>&1
+echo "Done."
+
 echo "LXC succesfully installed and configured for unprivileged containers!"
 
 exit 0
