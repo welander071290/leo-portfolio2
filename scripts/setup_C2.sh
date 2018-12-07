6d46dd249059b8d4e5433d009e347a0cf6663625
welander071290 <welander071290@gmail.com>
2018-12-07 10:15:23 +0100

    setup_C2.sh code fix and tested

--- scripts/setup_C2.sh
+++ scripts/setup_C2.sh
@@ -7,19 +7,34 @@
 # Anders Welander
 #####################################################
 
-if lxc-create -n C2 -t download -- -d alpine -r 3.4 -a armhf ; then
-echo "Linux container C2 was created succesfully"
-lxc-start -n C2
+if lxc-ls | grep C2 ; then
+	echo "C2 already exist"
 else
-echo "The container could not be created, this program will terminate"
-exit 1
+	if lxc-create -n C2 -t download -- -d alpine -r 3.4 -a armhf ; then
+		echo "Linux container C2 was created succesfully"
+	else
+		echo "The container could not be created, this program will terminate"
+		exit 1
+	fi
 fi
 
-lxc-attach -n C2 --
-echo "You are now working inside the container"
-apk update
-apk add socat
+lxc-start -n C2
+echo "Caontainer started"
+
+#This will try to update 3 times
+n=0
+	until [ $n -ge 3 ]
+	do
+		lxc-attach -n C2 -- apk update & break
+		n=$[$n+1]
+	done
+
+echo "Container C2 is updated"
+
+lxc-attach -n C2 -- apk add socat
+echo "Socat installed"
 
 
-lxc-attach -n C2 -- socat -v -v tcp-listen:8080,fork,reuseaddr exec:/bin/random.sh
+lxc-attach -n C2 -- socat -v -v tcp-listen:8080,fork,reuseaddr exec:/random.sh
 
+exit
